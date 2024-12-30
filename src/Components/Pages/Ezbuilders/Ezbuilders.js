f60e6ed2d09e8e72af6818490836b1abd7d700c4
eldalland <70651244+eldalland@users.noreply.github.com>
2024-12-30 15:09:32 -0500

    feat: shrank fonts on hpage hroizontal m version

--- src/Components/Pages/Ezbuilders/Ezbuilders.js
+++ src/Components/Pages/Ezbuilders/Ezbuilders.js
@@ -319,7 +319,7 @@ const Ezbuilders = () => {
 
   return (
     <>
-    <div className={genstyles.box}>
+   
       <NavbarEZ text="E-Z BUILDERS" />
       <div className={isFullScreen ? genstyles.close : genstyles.hidden}>
         <img src={CloseIcon} style={{width:"40px",}} onClick={close} />
@@ -374,7 +374,7 @@ const Ezbuilders = () => {
           ))}
         </div>
       </div>
-      </div>
+   
     </>
 
   );
