1f7ec6b57edd31292b4970ec580c44241ee12ff4
eldalland <eldalland@gmail.com>
2025-03-05 17:18:03 -0500

    Added API key to env

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -7,14 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9387XEkxsuhzMwztx6y3O6Ws4ezvaAds",
-  authDomain: "tdallandportfoliosite2024.firebaseapp.com",
-  projectId: "tdallandportfoliosite2024",
-  storageBucket: "tdallandportfoliosite2024.appspot.com",
-  messagingSenderId: "139178529478",
-  appId: "1:139178529478:web:ec216af23a54d55055bdc7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
